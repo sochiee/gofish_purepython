a60f0105dd35fafc323d52f072336bd847e2c392
rflsg <softsailors84@ciencias.unam.mx>
2026-06-11 20:17:20 -0600

    feat: pearson y error

--- linear_regression.py
+++ linear_regression.py
@@ -33,6 +33,31 @@ def olr(data: list[list]) -> list[float]:
     return explanatory_vars
 
 
+def calc_error(pred: list[float], truth: list[float]) -> float:
+    """Calcula el error entre la predicción y los datos"""
+    n = len(pred)
+    error = sum([(p - t) ** 2 for p, t in zip(pred, truth)]) / n
+    return error
+
+
+def pearson(regressors: list[float], response: list[float]) -> float:
+    """Calcula el coeficiente de Pearson"""
+    n = len(regressors)
+
+    suma_x = sum(regressors)
+    suma_y = sum(response)
+    suma_x2 = sum([xi**2 for xi in regressors])
+    suma_y2 = sum([yi**2 for yi in response])
+    suma_xy = sum([xi * yi for xi, yi in zip(regressors, response)])
+
+    num = (n * suma_xy) - (suma_x * suma_y)
+    den = ((n * suma_x2 - suma_x**2) * (n * suma_y2 - suma_y**2)) ** 0.5
+
+    coef = num / den
+
+    return coef
+
+
 if __name__ == "__main__":
     A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     print(olr(A))
